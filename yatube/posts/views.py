f0def7541e200994527d016a0f75f59cff1924c3
Артем Меркулов <artem-merk96@yandex.ru>
2022-01-28 08:57:47 +0300

    First review

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -150,9 +150,8 @@ def follow_index(request):
 @login_required
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
-    if request.user != author:
+    if author != request.user:
         Follow.objects.create(user=request.user, author=author)
-        Follow.objects.count() == 1
     return redirect('posts:profile', username)
 
 
